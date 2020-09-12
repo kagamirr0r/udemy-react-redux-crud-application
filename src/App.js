b5adbed3cf0c0bd3ac78460bc83eb6a839b1390c
kagami ryohei <a7112069@gmail.com>
2020-09-13 01:39:04 +0900

    Learn about Props

--- src/App.js
+++ src/App.js
@@ -31,18 +31,40 @@ import React from "react";
 // }
 
 const App = () => {
+  const profiles = [
+    {
+      name: "Taro",
+      age: 10,
+    },
+    {
+      name: "Hanako",
+      age: 5,
+    },
+    {
+      name: "No-Name",
+    },
+  ];
+  return (
+    <>
+      {/* <User name={"Taro"} age={10} />
+      <User name={"Hanako"} age={5} /> */}
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
+    </>
+  );
+};
+
+const User = (props) => {
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      I am {props.name}, and {props.age} years old.
     </div>
   );
 };
 
-const Cat = () => {
-  return <div>Meow!</div>;
+User.defaultProps = {
+  age: 1,
 };
 
 // function App() {
