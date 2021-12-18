ac93b09ad9df1443c02bf7474fc1001e4df57435
kagamirr0r <45422771+kagamirr0r@users.noreply.github.com>
2021-12-19 01:32:55 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Hello World!!</h1>;
+  const greeting = "Hello World";
+  const dom1 = <h1>Hi!</h1>;
+  const dom2 = <h1 className="foo">{greeting}</h1>;
+  // return dom2;
+
+  // return (
+  //   <input
+  //     type="text"
+  //     onClicked={() => {
+  //       console.log("I was clicked");
+  //     }}
+  //   />
+  // );
+
+  return (
+    <>
+      <label htmlFor="bat">bar</label>
+      <input
+        type="text"
+        onChange={() => {
+          console.log("I was changed");
+        }}
+      />
+    </>
+  );
 };
 
 export default App;
