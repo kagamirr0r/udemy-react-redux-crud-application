82daac5e4c5ee4ca59d1126388a59e5a0594cc67
kagamirr0r <45422771+kagamirr0r@users.noreply.github.com>
2021-12-19 17:49:39 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  const greeting = "Hello World";
-  const dom1 = <h1>Hi!</h1>;
-  const dom2 = <h1 className="foo">{greeting}</h1>;
-  // return dom2;
+// const App = () => {
+//   return (
+//     <>
+//       <label htmlFor="bat">bar</label>
+//       <input
+//         type="text"
+//         onChange={() => {
+//           console.log("I was changed");
+//         }}
+//       />
+//     </>
+//   );
+// };
 
-  // return (
-  //   <input
-  //     type="text"
-  //     onClicked={() => {
-  //       console.log("I was clicked");
-  //     }}
-  //   />
-  // );
-
-  return (
-    <>
-      <label htmlFor="bat">bar</label>
-      <input
-        type="text"
-        onChange={() => {
-          console.log("I was changed");
-        }}
-      />
-    </>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <>
+        <label htmlFor="bat">bar</label>
+        <input
+          type="text"
+          onChange={() => {
+            console.log("I was changed");
+          }}
+        />
+      </>
+    );
+  }
+}
 
 export default App;
